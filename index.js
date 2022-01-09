1b5fa8561ab8c020c0ed083cc79194bc12021943
= <rtyenriques@gmail.com>
2022-01-08 18:57:24 -0800

    commit tree fa8a79e12f2d7697e0f175b9eed3ed6df7d43e8a

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = sourceArray.map(function(num) {
+        return num * -1
+    }
+    )
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = sourceArray.map(function(num) {
+        return num * 2
+    })
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = sourceArray.map(function(num){
+        return num**2
+    })
+    return newArray
+}
+
+// const mapToSquare = (sourceArray) => {
+//     const squareArray = []; 
+//     for(let x of sourceArray){
+//         squareArray.push(x = x**2) 
+//     }
+//     return squareArray 
+// }
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    return sourceArray.reduce(reducer, startingPoint)
+}
+
+// const reduceToTotal = (sourceArray, startingPoint=0) => {
+//     let total = startingPoint
+//     for(let num of sourceArray){
+//         total = total + num 
+//     }
+//     return total
+// }
+
+function reduceToAllTrue(sourceArray) {
+    let value = true
+    for (let item of sourceArray){
+        if (item === false) {
+            value = false
+        }
+    }
+   return value
+}
+
+// const reduceToAllTrue = (sourceArray) => {
+//     let value = true 
+//     for(let item of sourceArray){
+//         if (item === false){
+//             value = false 
+//         }
+//     }
+//     return value 
+// }
+
+function reduceToAnyTrue(sourceArray) {
+    let value = false;
+    for(let item of sourceArray){
+        if (item === true) {
+            value = true
+        }
+    }
+    return value
+}
+
+// const reduceToAnyTrue = (sourceArray) => {
+//     let value = false 
+//     for(let item of sourceArray){
+//         if (item === true){
+//             value = true 
+//         }
+//     }
+//     return value 
+// }
